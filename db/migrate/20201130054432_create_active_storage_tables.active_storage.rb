0a3f4d5fb528f3d7afd9b358f4e36e0c7a969832
shane tully <shane@shanetully.com>
2020-12-01 00:18:42 -0800

    add photo uploading

---
+++ db/migrate/20201130054432_create_active_storage_tables.active_storage.rb
@@ -0,0 +1,24 @@
+# This migration comes from active_storage (originally 20170806125915)
+class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
+  def change
+    create_table :active_storage_blobs do |table|
+      table.string :key, null: false
+      table.string :filename, null: false
+      table.string :content_type
+      table.text :metadata
+      table.bigint :byte_size, null: false
+      table.string :checksum, null: false
+      table.datetime :created_at, null: false
+      table.index [:key], unique: true
+    end
+
+    create_table :active_storage_attachments do |table|
+      table.string :name, null: false
+      table.references :record, null: false, polymorphic: true, index: false
+      table.references :blob, null: false
+      table.datetime :created_at, null: false
+      table.index [:record_type, :record_id, :name, :blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
+      table.foreign_key :active_storage_blobs, column: :blob_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_003707) do
+ActiveRecord::Schema.define(version: 2020_12_01_052808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "airports", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +62,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_003707) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "landing_rights"
     t.string "landing_requirements"
+    t.string "diagram"
     t.index ["code"], name: "index_airports_on_code", unique: true
     t.index ["site_number"], name: "index_airports_on_site_number", unique: true
   end
@@ -73,6 +95,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_003707) do
     t.index ["airport_id"], name: "index_tags_on_airport_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "remarks", "airports"
   add_foreign_key "runways", "airports"
   add_foreign_key "tags", "airports"
