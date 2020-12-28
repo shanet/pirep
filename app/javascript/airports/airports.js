a5cfa43fb385584ab58c6f5dc160675c9904c5a6
shane tully <shane@shanetully.com>
2020-12-27 20:40:25 -0800

    style remarks on airport show page

--- app/javascript/airports/airports.js
+++ app/javascript/airports/airports.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   initEditingTags();
   initTagDeleteIcons();
+  initExtraRemarks();
 });
 
 function initEditingTags() {
@@ -12,6 +13,7 @@ function initEditingTags() {
     tag.addEventListener('click', () => {
       tag.classList.toggle('unselected');
 
+      // When a tag is selected set its associated form field as selected too
       let selectedFormField = document.getElementById(`airport_tags_attributes_${i}_selected`);
       selectedFormField.value = (selectedFormField.value === 'true' ? 'false' : 'true');
     });
@@ -22,9 +24,29 @@ function initTagDeleteIcons() {
   let addTags = document.querySelector('.tag-square.add');
   let deleteIcons = document.querySelectorAll('.tag-square .delete');
 
+  if(!addTags) return;
+
   addTags.addEventListener('click', () => {
+    // Show/hide the tag delete icons
     for(let i=0; i<deleteIcons.length; i++) {
       deleteIcons[i].style.display = (['none', ''].indexOf(deleteIcons[i].style.display) !== -1 ? 'block' : 'none');
+    }
+  });
+}
+
+function initExtraRemarks() {
+  let showExtraRemarks = document.getElementById('show-extra-remarks');
+  let extraRemarks = document.querySelectorAll('.extra-remark');
+
+  if(!showExtraRemarks) return;
+
+  showExtraRemarks.addEventListener('click', () => {
+    // Update the button text accordingly
+    showExtraRemarks.innerText = (extraRemarks[0].classList.contains('hidden') ? 'Hide More' : 'Show More');
+
+    // Show/hide the extra remarks
+    for(let i=0; i<extraRemarks.length; i++) {
+      extraRemarks[i].classList.toggle('hidden');
     }
   });
 }
--- app/models/remark.rb
+++ app/models/remark.rb
@@ -1,3 +1,7 @@
 class Remark < ApplicationRecord
   belongs_to :airport
+
+  def to_human_readable
+    return text.downcase.capitalize
+  end
 end
