0d0fcb2f79294c230d4743a7a701789608b2937d
shane tully <shane@shanetully.com>
2022-11-07 00:03:32 -0800

    fix #11: add sentry

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,14 @@
+// JavaScript
+// ---------------------------------------------
 //= link_tree ../javascripts .js
 //= link_tree ../../../vendor/javascript .js
 
+
+// CSS
+// ---------------------------------------------
 //= link application.css
 
+
+// Images
+// ---------------------------------------------
 //= link_tree ../images .png
