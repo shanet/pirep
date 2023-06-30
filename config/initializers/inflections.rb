6f52e4975498108d6cde0413d3b85a0422539eea
shane tully <shane@shanetully.com>
2023-06-30 13:26:54 -0700

    fix inflection for singular "military bases" facility type

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -4,5 +4,6 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   # inflect.irregular 'person', 'people'
   # inflect.uncountable %w( fish sheep )
 
+  inflect.singular 'Military Bases', 'Military Base'
   inflect.singular 'Seaplane Bases', 'Seaplane Base'
 end
