a6ac2947fb7e3f4ec9af55483eebc0184bdc21c3
shane tully <shane@shanetully.com>
2023-07-15 14:32:03 -0700

    fix redirect for tags in versions update route

--- test/controllers/manage/versions_controller_test.rb
+++ test/controllers/manage/versions_controller_test.rb
@@ -6,7 +6,7 @@ class Manage::VersionsControllerTest < ActionDispatch::IntegrationTest
     sign_in :admin
   end
 
-  test 'update' do
+  test 'update airport version' do
     with_versioning do
       @airport.update!(description: 'making a version')
 
@@ -14,6 +14,17 @@ class Manage::VersionsControllerTest < ActionDispatch::IntegrationTest
       assert_redirected_to history_airport_path(@airport)
 
       assert_in_delta Time.zone.now, @airport.versions.last.reviewed_at, 3.seconds, 'Version not set as reviewed'
+    end
+  end
+
+  test 'update tag version' do
+    with_versioning do
+      tag = Tag.create!(name: :camping, airport: @airport)
+
+      patch manage_version_path(tag.versions.last), params: {version: {reviewed_at: Time.zone.now}}
+      assert_redirected_to history_airport_path(@airport)
+
+      assert_in_delta Time.zone.now, tag.versions.last.reviewed_at, 3.seconds, 'Version not set as reviewed'
     end
   end
 end
