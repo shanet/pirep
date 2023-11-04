5ee4767c5f51603406dbf3d8330682b90552eac4
shane tully <shane@shanetully.com>
2023-11-03 22:28:14 -0700

    fix event system test

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -2,7 +2,7 @@ require 'application_system_test_case'
 
 class EventsTest < ApplicationSystemTestCase
   setup do
-    @event = create(:event)
+    @event = create(:event, :recurring)
   end
 
   test 'edits event' do
