686c9242649b938511d565cdccf39c16fd8c6ebe
shane tully <shane@shanetully.com>
2023-11-04 12:27:30 -0700

    handle recurring events on the 4th and 5th weeks of a month

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -8,6 +8,12 @@ class EventsTest < ApplicationSystemTestCase
   test 'edits event' do
     visit edit_event_path(@event)
 
+    # There should be two week-of-month options for the 4th/5th day in a month
+    fill_in 'event_start_date', with: DateTime.new(2023, 7, 22)
+    assert_equal ['day_22', 'week_4', 'week_-1'], all('#event_recurring_week_of_month > option').map(&:value)
+    fill_in 'event_start_date', with: DateTime.new(2023, 7, 29)
+    assert_equal ['day_29', 'week_5', 'week_-1'], all('#event_recurring_week_of_month > option').map(&:value)
+
     fill_in 'Description', with: 'Lorem ipsum'
     click_button 'Submit'
 
