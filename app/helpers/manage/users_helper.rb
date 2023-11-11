918e3a01233a0c1bd3385e56eaf5366281e62f49
shane tully <shane@shanetully.com>
2023-11-10 20:46:29 -0800

    show IPs on user manage page

--- app/helpers/manage/users_helper.rb
+++ app/helpers/manage/users_helper.rb
@@ -1,9 +1,11 @@
 module Manage::UsersHelper
-  def google_maps_location_link(ip_address)
+  def google_maps_location_link(ip_address, label=nil)
     geoip = MaxmindDb.client.geoip_lookup(ip_address)
     return 'Unknown' unless geoip
 
+    label ||= "#{geoip[:latitude]}, #{geoip[:longitude]}"
     url = "https://google.com/maps?q=#{geoip[:latitude]}%2C#{geoip[:longitude]}&ll=#{geoip[:latitude]}%2C#{geoip[:longitude]}&z=7"
-    return link_to("#{geoip[:latitude]}, #{geoip[:longitude]}", url, target: :_blank, rel: 'noopener')
+
+    return link_to(label, url, target: :_blank, rel: 'noopener')
   end
 end
