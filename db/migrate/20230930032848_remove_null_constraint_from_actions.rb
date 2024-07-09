b2c0c994fa4e139a0b2e895cb4ec7f05dafaf294
shane tully <shane@shanetully.com>
2024-07-09 16:14:08 -0700

    update gems for rack CVE

--- db/migrate/20230930032848_remove_null_constraint_from_actions.rb
+++ db/migrate/20230930032848_remove_null_constraint_from_actions.rb
@@ -1,6 +1,6 @@
 class RemoveNullConstraintFromActions < ActiveRecord::Migration[7.0]
   def change
-    change_column_null :actions, :actionable_id, true
+    change_column_null :actions, :actionable_id, true # rubocop:disable Rails/BulkChangeTable
     change_column_null :actions, :actionable_type, true
   end
 end
