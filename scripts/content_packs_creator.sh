b84647cc7fda9f10813f217ba9faf232adb74295
shane tully <shane@shanetully.com>
2025-01-10 18:01:34 -0800

    background puma in content packs creator script

--- scripts/content_packs_creator.sh
+++ scripts/content_packs_creator.sh
@@ -4,6 +4,12 @@
 # pack service as we need a container with more memory than the jobs container to process map tiles.
 
 # The content pack creator needs a web server running so Puppeteer can get airport snapshots
-puma --config config/puma.rb
+puma --config config/puma.rb &
+WEB_SERVER_PID=$!
 
 rails runner "ContentPacksCreator.new.create_content_packs"
+
+# Kill the web server process once done (give it some time to gracefully exit and then sigkill it to ensure the ECS task stops)
+kill $WEB_SERVER_PID
+sleep 30
+kill -9 $WEB_SERVER_PID
