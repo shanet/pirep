0e7c00206af152e1685ace0f88017c494ca90e69
shane tully <shane@shanetully.com>
2025-01-10 18:03:09 -0800

    sort exceptions

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -9,11 +9,11 @@ module Exceptions
   class DatabaseBackupFailed < StandardError; end
   class DiagramUploadFailed < StandardError; end
   class EaaEventsFetchFailed < StandardError; end
+  class EcsTaskRunnerFailed < StandardError; end
   class GdalBinaryNotFound < StandardError; end
   class GooglePhotosQueryFailed < StandardError; end
   class GoogleTimezoneQueryFailed < StandardError; end
   class IncompleteLocationFilter < StandardError; end
-  class EcsTaskRunnerFailed < StandardError; end
   class MaxmindDatabaseChecksumDownloadFailed < StandardError; end
   class MaxmindDatabaseDownloadFailed < StandardError; end
   class MaxmindDatabaseIntegrityCheckFailed < StandardError; end
