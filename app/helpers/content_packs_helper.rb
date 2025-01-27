45fb8692a5a4e0766ae564a4273f6cb7fcc5f651
shane tully <shane@shanetully.com>
2025-01-27 01:01:53 -0800

    serve content packs directly

--- app/helpers/content_packs_helper.rb
+++ app/helpers/content_packs_helper.rb
@@ -9,14 +9,13 @@ module ContentPacksHelper
     return ContentPacksCreator.icons
   end
 
-  def content_path_url(content_pack_id)
+  def content_pack_versioned_url(content_pack_id)
     # Add the timestamp of the current content pack as a query param so if the CDN is used we bust its cache when a new content pack is issued
-    url = content_pack_path(content_pack_id, version: File.basename(ContentPacksCreator.path_for_content_pack(content_pack_id) || ''))
+    url = content_pack_path(content_pack_id, version: File.basename(ContentPacksCreator.path_for_content_pack(content_pack_id) || '').split('_').last)
 
     # Serve the content pack from the CDN if its configured
     url = "#{Rails.configuration.action_controller.asset_host}/#{url}" if Rails.configuration.action_controller.asset_host.present?
 
-    # If on iOS offer a link that redirects through ForeFlight's website so the content pack opens directly in the app
-    return (ios?(request) ? "https://foreflight.com/content?downloadURL=#{url}" : url)
+    return url
   end
 end
