53f9454438e17515b87a955f4336dab43034f185
kira tully <kira@kiratully.com>
2026-03-08 17:36:39 -0700

    try reducing ci wait times

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -19,7 +19,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   end
 
   # CI on GitHub actions runs wayyyyyyy slower due to lack of GPU acceleration and Mapbox needing WebGL so give it more time before failing a test
-  Capybara.default_max_wait_time = 60 if ENV['CI']
+  Capybara.default_max_wait_time = 30 if ENV['CI']
 
   # We'll get far fewer flakes without animations
   Capybara.disable_animation = true
@@ -29,8 +29,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   def wait_for_map_ready(id='map')
     # Once the map is fully ready to use it will populate a data attribute
-    # Mapbox runs incredibly slow on CI so give it a bunch of time before failing
-    find("##{id}[data-ready=\"true\"]", wait: (ENV['CI'] ? 300 : 60))
+    find("##{id}[data-ready=\"true\"]", wait: 60)
   rescue Capybara::ElementNotFound => error
     # Something may have prevented Mapbox from initalizing, it would he helpful to print the browser logs in this case
     warn page.driver.browser.logs.get(:browser)
